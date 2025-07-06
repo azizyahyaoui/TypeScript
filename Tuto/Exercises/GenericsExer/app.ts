05b0ea89b120eedb981c5ef3ee3421a15a0eaf1f
Yahyaoui Med Aziz <azizyahyaoui716@gmail.com>
2025-07-06 23:01:32 +0100

    Add async function implementation and user data fetching example

--- Tuto/Exercises/GenericsExer/app.ts
+++ Tuto/Exercises/GenericsExer/app.ts
@@ -23,3 +23,32 @@ printArraysContents(myArrays)
 
 myArrays = ["ABC","DEF","GHI","JKL"]
 printArraysContents(myArrays)
+
+
+// Let's test async function 
+
+/* const asyncFunction = async ():string => { // The return type of an async function or method must be the global Promise<T> type. Did you mean to write 'Promise<string>'?"
+
+}
+ */
+
+// let's do it right now
+
+type Users = {
+    readonly id: number;
+    username: string;
+    email?: string;
+}
+
+
+const MyUsers: Users = {
+    id: 1,
+    username: "adminAdmin",
+    email: "admin@mail.xyz"
+}
+
+const asyncFunction = async (userData:Users): Promise<Users> =>{
+    return userData;
+}
+
+console.log(`Fetching user data: \n ${JSON.stringify(asyncFunction(MyUsers))}`);
